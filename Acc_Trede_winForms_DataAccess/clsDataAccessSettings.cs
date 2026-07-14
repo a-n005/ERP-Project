6a1ff72a901c4d4fc269e854d72df537a7cebfa7
A <anasal2018@gmail.com>
2026-07-14 06:18:52 +0300

    ref: move connection string to App.config XML and add test

--- Acc_Trede_winForms_DataAccess/clsDataAccessSettings.cs
+++ Acc_Trede_winForms_DataAccess/clsDataAccessSettings.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Acc_Trede_winForms_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static readonly string ConnectionString = "Server=.;Database=Acc_Trede;User Id=sa;Password=sa123456;";
+        // from giminai to add in config
+        //  connectionString="Server=.;Database=Acc_Trede;Trusted_Connection=True;TrustServerCertificate=True;"
 
+        //public static readonly string ConnectionString = "Server=.;Database=Acc_Trede;User Id=sa;Password=sa123456;";
+        public static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["DB_AccTrede"].ConnectionString;
     }
 }
---
+++ Acc_Trede_winForms_Test/Global/clsConnectionStringTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace Acc_Trede_winForms_Test.Global
+{
+    public class clsConnectionStringTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        // 2. Inject it through the constructor (xUnit does this automatically)
+        public clsConnectionStringTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        [Fact]
+        public void ConnectionString_Access_Success()
+        {
+            string x= ConfigurationManager.ConnectionStrings["DB_AccTrede"].ConnectionString;
+            _output.WriteLine("the var value: " + x);
+            Assert.False(string.IsNullOrEmpty(x), "Error a var not have a value.");
+            
+        }
+    }
+}
