a6f649941b27e7807c30e9fc253437e2432abdda
A <anasal2018@gmail.com>
2026-07-19 08:19:56 +0300

    feat(dal): implement clsPurchaseReturn,Details_DAL

---
+++ Acc_Trede_winForms_DataAccess/clsPurchaseReturnDetails_DAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acc_Trede_winForms_DataAccess
+{
+    public class clsPurchaseReturnDetails_DAL
+    {
+        public static SqlParameter InsertCart(DataTable dt)
+        {
+            SqlParameter tvpParameter=new SqlParameter("@Cart",SqlDbType.Structured);
+            tvpParameter.TypeName = "dbo.PutchaseCartType";
+            tvpParameter.Value = dt;
+            return tvpParameter;
+        }
+    }
+}
