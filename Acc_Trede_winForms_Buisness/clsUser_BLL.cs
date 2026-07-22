bb5797f8762b12e0ef36fdeaece38247aff9b50a
A <anasal2018@gmail.com>
2026-07-22 15:07:15 +0300

    wip(bll): initial draft for clsUser_BLL

---
+++ Acc_Trede_winForms_Buisness/clsUser_BLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acc_Trede_winForms_Buisness
+{
+    internal class clsUser_BLL
+    {
+    }
+}
