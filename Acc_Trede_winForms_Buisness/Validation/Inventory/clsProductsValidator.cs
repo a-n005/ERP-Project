cdd36118d07ad4885261b47dfdbf1a81e16a4a82
A <anasal2018@gmail.com>
2026-08-27 08:03:48 +0300

    feat(Validators): add FluentValidation for Finance

--- Acc_Trede_winForms_Buisness/Validation/Inventory/clsProductsValidator.cs
+++ Acc_Trede_winForms_Buisness/Validation/Inventory/clsProductsValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acc_Trede_winForms_Buisness.Inventory;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,39 @@ using System.Threading.Tasks;
 
 namespace Acc_Trede_winForms_Buisness.Validation.Inventory
 {
-    internal sealed class clsProductsValidator
+    internal sealed class clsProductsValidator : AbstractValidator<clsProducts_BLL>
     {
         public clsProductsValidator()
         {
-            
+            // barcode Validation
+            RuleFor(x => x.Barcode)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("يجب تسجيل باركود.");
+
+            // prodctName validation
+            RuleFor(x => x.ProductName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("يجب تسجيل اسم.");
+
+            // costPrice validtion
+            RuleFor(x => x.CostPrice)
+                .GreaterThanOrEqualTo(0).WithMessage("يجب ان تضاف قيمه التكلفة.");
+
+            // salePrice validation
+            RuleFor(x => x.SalePrice)
+                .GreaterThanOrEqualTo(0).WithMessage("يجب اضافه سعر بيع موجب.");
+
+            // quantity validation
+            RuleFor(x => x.Quantity)
+                .GreaterThanOrEqualTo(0).WithMessage(" لا تستطيع اضافه كميه سالبه.");
+
+            // mainQuantity Alert validation
+            RuleFor(x => x.MinQuantityAlert)
+                .GreaterThanOrEqualTo(0).WithMessage("يجب اضافه حد ادنى.");
         }
+
+       
     }
 }
